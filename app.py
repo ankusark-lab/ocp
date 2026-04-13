d009492e83c8e411c688690c75ee0a1219ce23be
ankusark-lab <ankusark@redhat.com>
2026-04-13 12:25:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     pod_name = os.getenv('HOSTNAME', 'Local-Machine')
-    return f"<h1>Build Successful! Demo done new patch for uat</h1><p>Running on Pod: {pod_name}</p>"
+    return f"<h1>Build Successful! Demo done new patch for uat ankush</h1><p>Running on Pod: {pod_name}</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3333)
